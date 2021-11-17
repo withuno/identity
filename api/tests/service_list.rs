7361975d4e9571602427dce314c4a3b3e11e68c6
Chris Eberly <ceberly@users.noreply.github.com>
2021-11-16 22:16:27 -0500

    add static services file and test

---
+++ api/tests/service_list.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+
+use serde_json::{Result, Value};
+use std::fs;
+
+#[test]
+fn service_list_is_valid() {
+    // try to deserialize the service list file and hope for the best!
+    let contents = fs::read_to_string("static/services.json").unwrap();
+
+    let v: Result<Value> = serde_json::from_str(&contents);
+
+    assert!(v.is_ok());
+}
