8d6fefced693d0bfb7a8a0105978ce439b72f430
Chris Eberly <ceberly@users.noreply.github.com>
2021-11-30 12:28:53 -0500

    services json is now built in the services repo

--- api/tests/service_list.rs
+++ api/tests/service_list.rs
@@ -6,7 +6,7 @@ use std::fs;
 #[test]
 fn service_list_is_valid() {
     // try to deserialize the service list file and hope for the best!
-    let contents = fs::read_to_string("static/services.json").unwrap();
+    let contents = fs::read_to_string("tests/services.json").unwrap();
 
     let v: Result<Value> = serde_json::from_str(&contents);
 
