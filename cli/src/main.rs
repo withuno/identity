5feeba1a1ce007bc799ea015d4f6497f0c6497e4
David Cowden <david@uno.app>
2022-03-24 12:04:02 -0600

    cli: add --conf option and plumb context
    With the introduction of global options each command needs access to them.
    This is accomplished via a context that is passed down to each command.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,18 +6,28 @@
 /// The uno utility is a cli frontend to operations that can be performed with
 /// an uno identity.
 
-use clap::Parser;
-use anyhow::{anyhow, bail, Context, Result};
+use clap::{Args, Parser,};
+use anyhow::{anyhow, bail, Context as AnyContext, Result,};
 use uno::Binding;
 
+use std::path::PathBuf;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 
 #[derive(Parser)]
 #[clap(version = "0.1", author = "David C. <david@uno.app>")]
 struct Opts {
-   #[clap(subcommand)]
-   subcmd: SubCommand, 
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+    #[clap(flatten)]
+    ctx: Context,
+}
+
+#[derive(Args)]
+struct Context {
+    /// Specify an optional config path to use instead of the default.
+    #[clap(long, parse(from_os_str), value_name = "PATH")]
+    conf: Option<PathBuf>,
 }
 
 #[derive(Parser)]
@@ -36,22 +46,20 @@ enum SubCommand {
     S39(S39Cmd),
 }
 
-
-
 /// Initialize Uno in your home environment. Data and config is stored under
 /// `~/.uno`. . 
 #[derive(Parser)]
 struct Init;
 
-fn do_init(_: Init) -> Result<String>
+fn do_init(ctx: Context, _: Init) -> Result<String>
 {
     // Check for existing config. If it exists, bail.
-    match load_conf() {
+    match load_conf(&ctx) {
         Ok(_) => bail!("uno already initialized"),
         _ => {},
     };
 
-    let conf_file = config_path()?;
+    let conf_file = config_path(&ctx)?;
     let uno_dir = match conf_file.parent() {
         Some(p) => p,
         None => bail!("file `{}` has no parent", conf_file.to_string_lossy()),
@@ -62,19 +70,23 @@ fn do_init(_: Init) -> Result<String>
     Ok(format!("wrote: {}", conf_file.to_string_lossy()))
 }
 
-fn config_path() -> Result<std::path::PathBuf>
+fn config_path(ctx: &Context) -> Result<std::path::PathBuf>
 {
-    let mut path = dirs_next::home_dir()
-        .ok_or(anyhow!("can't find home dir"))?;
-    path.push(".uno");
-    path.push("config");
-
-    Ok(path)
+    Ok(match ctx.conf {
+        Some(ref p) => p.to_path_buf(),
+        None => {
+            let mut home = dirs_next::home_dir()
+                .ok_or(anyhow!("can't find home dir"))?;
+            home.push(".uno");
+            home.push("config");
+            home
+        },
+    })
 }
 
-fn load_conf() -> Result<cli::Config>
+fn load_conf(ctx: &Context) -> Result<cli::Config>
 {
-    let conf_file = config_path()?;
+    let conf_file = config_path(ctx)?;
     cli::load_config(&conf_file)
 }
 
@@ -87,11 +99,11 @@ struct Seed
     ephemeral: bool
 }
 
-fn do_seed(s: Seed) -> Result<String>
+fn do_seed(ctx: Context, s: Seed) -> Result<String>
 {
     let id = match s.ephemeral {
         true => uno::Id::new(),
-        false => cli::load_seed(&load_conf()?)?,
+        false => cli::load_seed(&load_conf(&ctx)?)?,
     };
 
     Ok(base64::encode_config(id.0, base64::STANDARD_NO_PAD))
@@ -107,15 +119,17 @@ struct Pubkey
     seed: String,
 }
 
-fn do_pubkey(c: Pubkey) -> Result<String>
+fn do_pubkey(ctx: Context, c: Pubkey) -> Result<String>
 {
     let id = id_from_b64(c.seed)?;
     let key = uno::KeyPair::from(id);
     Ok(base64::encode(&key.public.as_bytes()))
 }
 
-/// AEAD open The decrypt operation works with both 32 byte identity seeds and
-/// the 8 byte Mu. The actual symmetric key is derived appropriate in each case.
+/// AEAD/Sealed-box open.
+///
+/// The decrypt operation works with both 32 byte identity seeds and the 8 byte
+/// Mu. The actual symmetric key is derived appropriate in each case.
 #[derive(Parser)]
 struct Decrypt
 {
@@ -135,7 +149,7 @@ struct Decrypt
     data: Option<String>,
 }
 
-fn do_decrypt(c: Decrypt) -> Result<String>
+fn do_decrypt(ctx: Context, c: Decrypt) -> Result<String>
 {
     let key: uno::SymmetricKey;
     if let Some(r) = c.seed {
@@ -171,8 +185,10 @@ fn do_decrypt(c: Decrypt) -> Result<String>
     Ok(String::from_utf8(data)?)
 }
 
-/// AEAD seal. The encrypt operation works with both 32 byte identity seeds and
-/// the 8 byte Mu. The actual symmetric key is derived appropriate in each case.
+/// AEAD/Box seal.
+///
+/// The encrypt operation works with both 32 byte identity seeds and the 8 byte
+/// Mu. The actual symmetric key is derived appropriate in each case.
 #[derive(Parser)]
 struct Encrypt
 {
@@ -194,7 +210,7 @@ struct Encrypt
     raw: bool
 }
 
-fn do_encrypt(c: Encrypt) -> Result<String>
+fn do_encrypt(ctx: Context, c: Encrypt) -> Result<String>
 {
     let key: uno::SymmetricKey;
     if let Some(r) = c.seed {
@@ -243,7 +259,7 @@ struct Sign
     seed: String,
 }
 
-fn do_sign(c: Sign) -> Result<String>
+fn do_sign(ctx: Context, c: Sign) -> Result<String>
 {
     use uno::Signer;
     let id = id_from_b64(c.seed)?;
@@ -272,7 +288,7 @@ struct Verify
     signature: String
 }
 
-fn do_verify(c: Verify) -> Result<String>
+fn do_verify(_: Context, c: Verify) -> Result<String>
 {
     use uno::Verifier;
 
@@ -316,7 +332,8 @@ struct Vault
     data: Option<String>,
 }
 
-fn do_vault(c: Vault) -> Result<String>
+// TODO make subcommand on method/action
+fn do_vault(ctx: Context, c: Vault) -> Result<String>
 {
     use http_types::Method;
     use std::str::FromStr;
@@ -346,7 +363,7 @@ fn do_vault(c: Vault) -> Result<String>
 #[derive(Parser)]
 struct Mu;
 
-fn do_mu(_: Mu) -> Result<String>
+fn do_mu(_: Context, _: Mu) -> Result<String>
 {
     let id = uno::Mu::new();
     Ok(base64::encode(id.0))
@@ -361,7 +378,7 @@ struct Session
     seed: String,
 }
 
-fn do_session(c: Session) -> Result<String>
+fn do_session(_: Context, c: Session) -> Result<String>
 {
     let mu = mu_from_b64(c.seed)?;
     let sid = uno::Session::try_from(mu)?;
@@ -392,7 +409,8 @@ struct Ssss
     data: Option<String>,
 }
 
-fn do_ssss(c: Ssss) -> Result<String>
+// TODO make subcommand keyed on method
+fn do_ssss(ctx: Context, c: Ssss) -> Result<String>
 {
     use http_types::Method;
     use std::str::FromStr;
@@ -459,16 +477,16 @@ struct S39Split
     seed: String,
 }
 
-fn do_s39(s: S39Cmd) -> Result<String>
+fn do_s39(ctx: Context, s: S39Cmd) -> Result<String>
 {
     match s.subcmd {
-        S39::Split(c)  => do_s39_split(c),
+        S39::Split(c)  => do_s39_split(ctx, c),
         S39::Combine(c) => do_s39_combine(c),
         S39::View(c) => do_s39_view(c),
     }
 }
 
-fn do_s39_split(c: S39Split) -> Result<String>
+fn do_s39_split(ctx: Context, c: S39Split) -> Result<String>
 {
     let id = id_from_b64(c.seed)?;
     let groups = uno::split(id, &[(c.minimum,c.total)])
@@ -557,19 +575,22 @@ fn do_s39_view(c: S39View) -> Result<String>
 
 fn main() -> Result<()>
 {
-    let out = match Opts::parse().subcmd {
-        SubCommand::Init(c) => do_init(c),
-        SubCommand::Seed(c) => do_seed(c),
-        SubCommand::Pubkey(c) => do_pubkey(c),
-        SubCommand::Decrypt(c) => do_decrypt(c),
-        SubCommand::Encrypt(c) => do_encrypt(c),
-        SubCommand::Sign(c) => do_sign(c),
-        SubCommand::Verify(c) => do_verify(c),
-        SubCommand::Vault(c) => do_vault(c),
-        SubCommand::Mu(c) => do_mu(c),
-        SubCommand::Session(c) => do_session(c),
-        SubCommand::Ssss(c) => do_ssss(c),
-        SubCommand::S39(c) => do_s39(c),
+    let cli = Opts::parse();
+    let ctx = cli.ctx;
+
+    let out = match cli.subcmd {
+        SubCommand::Init(cmd) => do_init(ctx, cmd),
+        SubCommand::Seed(cmd) => do_seed(ctx, cmd),
+        SubCommand::Pubkey(cmd) => do_pubkey(ctx, cmd),
+        SubCommand::Decrypt(cmd) => do_decrypt(ctx, cmd),
+        SubCommand::Encrypt(cmd) => do_encrypt(ctx, cmd),
+        SubCommand::Sign(cmd) => do_sign(ctx, cmd),
+        SubCommand::Verify(cmd) => do_verify(ctx, cmd),
+        SubCommand::Vault(cmd) => do_vault(ctx, cmd),
+        SubCommand::Mu(cmd) => do_mu(ctx, cmd),
+        SubCommand::Session(cmd) => do_session(ctx, cmd),
+        SubCommand::Ssss(cmd) => do_ssss(ctx, cmd),
+        SubCommand::S39(cmd) => do_s39(ctx, cmd),
     }?;
 
     println!("{}", out);
@@ -600,7 +621,5 @@ mod test
 
     #[test]
     fn test_id() {
-        // use:
-        // https://docs.rs/clap/3.0.0-beta.5/clap/struct.App.html#method.try_get_matches_from
     }
 }
