dc955a617aaf179836591d61b5394c73f0f34f73
Chris Eberly <ceberly@users.noreply.github.com>
2022-05-18 13:14:26 -0400

    implement the logic of when stores can and can't be retreived

--- api/src/magic_share.rs
+++ api/src/magic_share.rs
@@ -36,6 +36,18 @@ pub fn new_from_json(json: &[u8]) -> Result<MagicShare> {
     }
 }
 
+pub async fn find_by_id(db: &impl Database, id: &str) -> Result<MagicShare> {
+    for x in &[PREFIX_ONE_DAY, PREFIX_ONE_WEEK, PREFIX_ONE_MONTH] {
+        let key = format!("{}/{}", x, id);
+        match get_share(db, &key).await {
+            Ok(v) => { return Ok(v); },
+            Err(_) => ()
+        }
+    }
+
+    return Err(anyhow!("Not found"));
+}
+
 pub async fn get_share(
     db: &impl Database,
     location: &str,
@@ -54,9 +66,14 @@ pub async fn store_share(
     db: &impl Database,
     share: &MagicShare,
 ) -> Result<String> {
+    match find_by_id(db, &share.id).await {
+        Ok(_) => return Err(anyhow!("Item already exists")),
+        _ => (),
+    };
+
     let diff = share.expires_at - Utc::now();
     let expiration_prefix = if diff < Duration::days(1) {
-       PREFIX_ONE_DAY 
+        PREFIX_ONE_DAY
     } else if diff < Duration::days(7) {
         PREFIX_ONE_WEEK
     } else {
@@ -117,6 +134,82 @@ mod tests {
         let location = store_share(&db, &m1).await.unwrap();
         let r = get_share(&db, &location).await.unwrap();
         assert_eq!(m1.id, r.id);
+    }
+
+    #[cfg(not(feature = "s3"))]
+    #[async_std::test]
+    async fn test_find_by_id() {
+        let dir = tempfile::TempDir::new().unwrap();
+        let db = FileStore::new(dir.path(), "test", "v0").await.unwrap();
+
+        let m1 = MagicShare {
+            id: "1234".to_string(),
+            expires_at: Utc::now() + Duration::days(30),
+            schema_version: 0,
+            encrypted_credential: "5678".to_string(),
+        };
+
+        store_share(&db, &m1).await.unwrap();
+        let r = find_by_id(&db, &m1.id).await.unwrap();
+        assert_eq!(r.id, m1.id);
+
+        // find only non-expired items
+        let m2 = MagicShare {
+            id: "5678".to_string(),
+            expires_at: Utc::now() - Duration::days(30),
+            schema_version: 0,
+            encrypted_credential: "5678".to_string(),
+        };
+
+        store_share(&db, &m2).await.unwrap();
+        let r2 = find_by_id(&db, &m2.id).await;
+        assert!(r2.is_err());
+
+        // if an expired item exists with the same id as a
+        // non-expired item, return the non-expired one.
+        let m3 = MagicShare {
+            id: "5678".to_string(),
+            expires_at: Utc::now() + Duration::days(30),
+            schema_version: 0,
+            encrypted_credential: "5678".to_string(),
+        };
+
+        store_share(&db, &m3).await.unwrap();
+        let r3 = find_by_id(&db, &m3.id).await.unwrap();
+        assert_eq!(r3.id, m3.id);
+    }
+
+    #[cfg(not(feature = "s3"))]
+    #[async_std::test]
+    async fn test_no_duplicate_shares() {
+        let dir = tempfile::TempDir::new().unwrap();
+        let db = FileStore::new(dir.path(), "test", "v0").await.unwrap();
+
+        let mut m1 = MagicShare {
+            id: "1234".to_string(),
+            expires_at: Utc::now() + Duration::days(30),
+            schema_version: 0,
+            encrypted_credential: "5678".to_string(),
+        };
+
+        assert!(store_share(&db, &m1).await.is_ok());
+        assert!(store_share(&db, &m1).await.is_err());
+
+        // different expiration class doesn't matter
+        m1.expires_at = Utc::now() + Duration::hours(1);
+        assert!(store_share(&db, &m1).await.is_err());
+
+        // expired objects can be overwritten
+        let mut m2 = MagicShare {
+            id: "5678".to_string(),
+            expires_at: Utc::now() - Duration::days(30),
+            schema_version: 0,
+            encrypted_credential: "5678".to_string(),
+        };
+        assert!(store_share(&db, &m2).await.is_ok());
+
+        m2.expires_at = Utc::now() + Duration::days(30);
+        assert!(store_share(&db, &m2).await.is_ok());
     }
 
     #[cfg(not(feature = "s3"))]
